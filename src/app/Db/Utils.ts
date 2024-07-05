9b26c786dc41c0c737204392d8b40d2f79c739e7
jeffery959 <aaronjeffery529@gmail.com>
2024-07-04 21:32:15 -0400

    Another change

---
+++ src/app/Db/Utils.ts
@@ -0,0 +1,14 @@
+import axios from "axios"
+export const UrlLink:string ='https://reciepe-n.vercel.app'
+
+export const getRecipe = async(setRecipeList:any)=>{
+   await axios.get(`${UrlLink}/api/AllRecipe`)
+    .then((response)=>{
+     setRecipeList(response.data.recipeList)
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+return
+  }
+
